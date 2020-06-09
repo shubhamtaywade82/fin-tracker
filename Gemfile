c29e3b92423abd1e8c6aebbaeded5ac0e2aaebcb
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-06-09 14:29:08 +0530

    add stocks contrllerdisplay stock price in browser

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'iex-ruby-client'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-
+gem 'font-awesome-rails'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
