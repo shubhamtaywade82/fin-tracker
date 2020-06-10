b79334c57eb435f85bc326be574f85c08a64d269
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-06-10 20:08:16 +0530

    added  freinds  searcg form

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   get 'friends', to: 'users#friends'
-  get 'search_friend', to: 'friends#search'
+  get 'search_friend', to: 'users#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
