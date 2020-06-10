99c96f461848d7e5458a450b7f9506e108d0e0cd
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-06-10 20:54:13 +0530

    added search method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,32 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
   def friends
     @friends = current_user.friends
   end
+
   def search
-    render json: params[:friend]
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/f_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = 'No user found'
+          format.js { render partial: 'users/f_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = 'Please enter a friend name or email to search'
+        format.js { render partial: 'users/f_result' }
+      end
+    end
   end
 end
