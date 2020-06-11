c52467ea8da13abfd3ab7a01698d41e08fb20a16
shubhamtaywade82 <shubhamtaywade82@gmail.com>
2020-06-11 16:53:28 +0530

    credential change

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,16 +6,22 @@ class Stock < ApplicationRecord
   validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
+    # client = IEX::Api::Client.new(
+    #   publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+    #   endpoint: 'https://sandbox.iexapis.com/v1'
+    # )
     client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-      endpoint: 'https://sandbox.iexapis.com/v1'
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
+      secret_token: Rails.application.credentials.iex_client[:secret_token],
+      endpoint: 'https://cloud.iexapis.com/v1'
     )
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-    rescue => exception
-      return nil
+    rescue StandardError => e
+      nil
     end
   end
+
   def self.check_db(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
